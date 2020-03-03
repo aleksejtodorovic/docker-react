ae26ab0c6573a44960ff59e86473897f22991443
Aleksej Todorovic <aleksej.todorovic@gmail.com>
2020-03-03 21:59:30 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hi there, this should work!</h1>
+        <h1>This was changed on feature branch!</h1>
       </header>
     </div>
   );
